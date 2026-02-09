678a62c8474d7d4f956e8aa80a2c5e397e941cc8
Arnaud <arnaud.malapert@univ-cotedazur.fr>
2026-02-09 23:32:30 +0100

    add items data

---
+++ R/items.R
@@ -0,0 +1,14 @@
+#' Item sizes for Subset Sum Problem instances
+#'
+#' A numeric vector containing the sizes of items used to generate
+#' instances of the Subset Sum Problem (SSP) for educational purposes.
+#'
+#' @format A numeric vector where each element represents the size of an item.
+#'
+#' @details
+#' Subsets of these items can be selected to form SSP instances with different
+#' target capacities.
+#'
+#' @source
+#' Generated for the Flemme Olympique workshops at Terra Numerica.
+"items"
---
+++ data-raw/items.R
@@ -0,0 +1,2 @@
+items <- sort(c(5:25, 10:20), decreasing = TRUE)
+usethis::use_data(items, overwrite = TRUE)
